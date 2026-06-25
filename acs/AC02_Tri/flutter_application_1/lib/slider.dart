b1f660331337a0a247ca4c85d516febe9205bbd7
iago-af124 <rm16590@fiap.com.br>
2026-06-25 10:06:38 -0300

    anotação pro Alex n achar q usei IA
    Updated the escolhaSlider function to clarify the use of the function parameter.

--- acs/AC02_Tri/flutter_application_1/lib/slider.dart
+++ acs/AC02_Tri/flutter_application_1/lib/slider.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-Widget escolhaSlider(double valor, Function(double) aoMudar) {
+Widget escolhaSlider(double valor, Function(double) aoMudar) { //usei o function para rodar um void
   return Padding(
     padding: const EdgeInsets.all(20),
     child: Slider(
